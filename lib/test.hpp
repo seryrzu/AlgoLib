90cab83b8847a079f5fd306cf56a1e49c5467ca6
Andrey Bzikadze <seryrzu@gmail.com>
2018-09-30 14:18:44 +0300

    Initial setup

---
+++ lib/test.hpp
@@ -0,0 +1,9 @@
+//
+// Created by Andrew Bzikadze on 9/29/18.
+//
+
+#pragma once
+
+int test() {
+    return 1;
+}
---
+++ test/test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Andrew Bzikadze on 9/29/18.
+//
+
+#include "gtest/gtest.h"
+
+#include "test.hpp"
+
+
+TEST(TestTest, test1) {
+    ASSERT_EQ(test(), 1);
+}
