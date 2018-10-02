ee7880e88dc658b2263d699b123607a681995fc8
Andrey Bzikadze <seryrzu@gmail.com>
2018-10-02 23:07:45 +0300

    Updates for tests generators

--- test/sorting/tests_generator.py
+++ test/sorting/tests_generator.py
@@ -3,50 +3,40 @@ import random
 import argparse
 import sys
 
-
-sys.path.append(os.path.join(os.path.pardir, os.path.pardir, 'py_utils'))
+this_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(this_dir, os.path.pardir, os.path.pardir, 'py_utils'))
 from os_utils import smart_makedirs
 from tests_hpp_generator import tests_hpp_generator
-
-
-def smart_makedirs(dirname):
-    import errno
-
-    try:
-        os.makedirs(dirname)
-    except OSError as exc:
-        if exc.errno != errno.EEXIST:
-            raise exc
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--outdir", help="output directory", default='tests')
-    parser.add_argument("-N", help="Number of tests", default=100, type=int)
+    parser.add_argument("-N", help="Number of tests", default=50, type=int)
     parser.add_argument("--min-elem", help="Min element", default=-10000, type=int)
     parser.add_argument("--max-elem", help="Min element", default=+10000, type=int)
     parser.add_argument("--min-size", help="Min sample size", default=1000, type=int)
-    parser.add_argument("--max-size", help="Min sample size", default=1000000, type=int)
+    parser.add_argument("--max-size", help="Min sample size", default=100000, type=int)
     parser.add_argument("--seed", help="Seed for simulations", default=random.randint(0, 1000000), type=int)
     params = parser.parse_args()
 
     random.seed(params.seed)
+    params.outdir = os.path.join(this_dir, params.outdir)
     smart_makedirs(params.outdir)
 
 
     filenames = []
     for i in range(params.N):
-        print(i + 1, params.N)
         sample_len = random.randint(params.min_size, params.max_size)
         sample = [str(random.randint(params.min_elem, params.max_elem)) for j in range(sample_len)]
         filename = 'sortingtest_%d_seed_%d_samplesize_%d.txt' % (i, params.seed, sample_len)
+        filename = os.path.abspath(os.path.join(params.outdir, filename))
         filenames.append(filename)
 
-        with open(os.path.join(params.outdir, filename), 'w') as f:
+        with open(filename, 'w') as f:
             print(' '.join(sample), file=f)
 
-    tests_hpp_generator(filenames, 'tests_fn.hpp', 'algolib::sorting')
-
+    tests_hpp_generator(filenames, os.path.join(this_dir, 'tests_fn.hpp'), 'algolib::sorting')
 
 
 if __name__ == "__main__":
