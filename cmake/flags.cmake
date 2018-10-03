91909bf4f8582407a8f2387efef86ebba43f9495
Andrey Bzikadze <seryrzu@gmail.com>
2018-10-04 00:37:35 +0300

    Prototype for flags in CMake

---
+++ cmake/flags.cmake
@@ -0,0 +1,25 @@
+add_definitions(-Wall -Wextra -Wconversion -Wno-sign-conversion -Wno-long-long -Wwrite-strings)
+add_definitions(-Wall
+                -Wextra
+                -Wshadow
+                -Wformat=2
+                -Wfloat-equal
+                -Wconversion
+                -Wlogical-op
+                -Wno-sign-conversion
+                -Wno-long-long
+                -Wwrite-strings)
+
+if (${CMAKE_BUILD_TYPE} STREQUAL "Debug")
+    message("Making Debug Configuration...")
+
+    add_definitions(-g3)
+    add_definitions(-D_GLIBCXX_DEBUG)
+endif()
+
+if (${CMAKE_BUILD_TYPE} STREQUAL "RelWithDebInfo")
+    message("Making Release Configuration...")
+
+    add_definitions(-g0)
+    add_definitions(-O2)
+endif()
